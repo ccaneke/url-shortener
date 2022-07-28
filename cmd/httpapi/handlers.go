82b2ea03d05b8a765d50aa3b38ae3c343118028e
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2022-07-28 16:51:10 +0200

    write send handler

---
+++ cmd/httpapi/handlers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func send(w http.ResponseWriter, r http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
