827d24769f5a20c1bdc34bc7dd981271fe3b4484
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2022-07-31 14:10:02 +0200

    update shorten function and add save, sync, and getURL functions

--- cmd/httpapi/handlers.go
+++ cmd/httpapi/handlers.go
@@ -1,48 +1,131 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
+
+	"golang.org/x/exp/maps"
 
 	"github.com/ccaneke/url-shortner-poc/internal"
 )
 
 const (
-	colon string = ":"
+	separator string = ":"
+	fileName  string = "mapping.json"
+
+	internalServerError = "Internal Server Error"
 )
+
+var mapping map[string]string = make(map[string]string)
 
 func shorten(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	log.Println("url: ", r.Host)
 
-	body, err := io.ReadAll(r.Body)
+	u, err := getURL(w, r.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, internalServerError, http.StatusInternalServerError)
 		return
 	}
 
-	u, err := url.Parse(string(body))
 	copy := *u
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	s := strings.Split(r.Host, colon)
-	domain := s[0]
+	subStrings := strings.Split(r.Host, separator)
+	domain := subStrings[0]
 	shortenedUrl, err := internal.ShortenURL(copy, domain)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	log.Printf("url from request body: %v, shortened url: %v", u, shortenedUrl)
+	mapping[shortenedUrl] = u.String()
+
+	projectPath, err := os.Getwd()
+	if err != nil {
+		http.Error(w, internalServerError, http.StatusInternalServerError)
+		return
+	}
+
+	absFilePath := projectPath + "/" + fileName
+	if _, err := os.Stat(absFilePath); errors.Is(err, os.ErrNotExist) {
+		err = save(absFilePath, mapping)
+		if err != nil {
+			http.Error(w, internalServerError, http.StatusInternalServerError)
+		}
+		return
+	}
+
+	m, err := sync(absFilePath, mapping)
+	if err != nil {
+		http.Error(w, internalServerError, http.StatusInternalServerError)
+	}
+
+	err = save(absFilePath, m)
+	if err != nil {
+		http.Error(w, internalServerError, http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(shortenedUrl))
+}
+
+func save(file string, changes map[string]string) error {
+	b, err := json.Marshal(changes)
+	if err != nil {
+		log.Println("save:", err)
+		return err
+	}
+
+	err = os.WriteFile(file, b, 0755)
+	if err != nil {
+		log.Println("save:", err)
+		return err
+	}
+
+	return nil
+}
+
+func sync(file string, changes map[string]string) (map[string]string, error) {
+	b, err := os.ReadFile(file)
+	if err != nil {
+		log.Println("sync:", err)
+		return nil, err
+	}
+
+	var m map[string]string
+
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		log.Println("sync: ", err)
+		return nil, err
+	}
+
+	maps.Copy(m, changes)
+
+	return m, nil
+}
+
+func getURL(w http.ResponseWriter, body io.ReadCloser) (*url.URL, error) {
+	b, err := io.ReadAll(body)
+	if err != nil {
+		log.Println("getURL: ", err)
+		return nil, err
+	}
+	defer body.Close()
+
+	u, err := url.Parse(string(b))
+	if err != nil {
+		log.Println("getURL", err)
+		return nil, err
+	}
+
+	return u, nil
 }
